f141375b45df904d9f5544e3e668a49c444ce800
Michael Muller <mmuller@google.com>
2019-08-30 10:46:29 -0400

    Output command test output as well as consuming it
    CommandTestCase currently consumes stdout & stderr for the command being
    tested.  Unfortunately, this results in us not being able to see the command
    output.  Add an output splitter so that output gets written to the original
    stream in addition to being captured.
    
    A simpler approach would be to print the captured data after command
    completion.  However, this won't work for tests that become hung and also
    won't display results in real-time.
    
    Tested: Ran a command test with verboseTestOutput=true, verified that standard
    output was visible.

--- core/src/test/java/google/registry/tools/CommandTestCase.java
+++ core/src/test/java/google/registry/tools/CommandTestCase.java
@@ -36,6 +36,7 @@ import google.registry.tools.params.ParameterFactory;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.List;
 import org.junit.Before;
@@ -75,8 +76,8 @@ public abstract class CommandTestCase<C extends Command> {
     // Ensure the UNITTEST environment has been set before constructing a new command instance.
     RegistryToolEnvironment.UNITTEST.setup(systemPropertyRule);
     command = newCommandInstance();
-    System.setOut(new PrintStream(stdout));
-    System.setErr(new PrintStream(stderr));
+    System.setOut(new PrintStream(new OutputSplitter(System.out, stdout)));
+    System.setErr(new PrintStream(new OutputSplitter(System.err, stderr)));
   }
 
   void runCommandInEnvironment(RegistryToolEnvironment env, String... args) throws Exception {
@@ -219,6 +220,52 @@ public abstract class CommandTestCase<C extends Command> {
           new TypeToken<C>(getClass()) {}.getRawType().getDeclaredConstructor().newInstance();
     } catch (InstantiationException | IllegalAccessException e) {
       throw new RuntimeException(e);
+    }
+  }
+
+  /**
+   * Splits an output stream, writing it to two other output streams.
+   *
+   * We use this as a replacement for standard out/error so that we can both capture the output of
+   * the command and display it to the console for debugging.
+   */
+  static class OutputSplitter extends OutputStream {
+
+    OutputStream a, b;
+
+    OutputSplitter(OutputStream a, OutputStream b) {
+      this.a = a;
+      this.b = b;
+    }
+
+    @Override
+    public void write(byte[] data) throws IOException {
+      a.write(data);
+      b.write(data);
+    }
+
+    @Override
+    public void write(byte[] data, int off, int len) throws IOException {
+      a.write(data, off, len);
+      b.write(data, off, len);
+    }
+
+    @Override
+    public void close() throws IOException {
+      a.close();
+      b.close();
+    }
+
+    @Override
+    public void flush() throws IOException {
+      a.flush();
+      b.flush();
+    }
+
+    @Override
+    public void write(int val) throws IOException {
+      a.write(val);
+      b.write(val);
     }
   }
 }
